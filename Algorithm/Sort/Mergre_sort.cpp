4cb56f55539054516076a8765ff83542695f3082
duymanhdoan <doanmanhduy.yb0210@gmail.com>
2021-08-19 17:18:50 +0700

    update bubble, merge, insertion sort

---
+++ Algorithm/Sort/Mergre_sort.cpp
@@ -0,0 +1,139 @@
+﻿#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+typedef long long ll;
+typedef long double ld;
+typedef complex<ld> cd;
+ 
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pl;
+typedef pair<ld,ld> pd;
+ 
+typedef vector<int> vi;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<cd> vcd;
+ 
+#define FOR(i, a, b) for (int i=a; i<(b); i++)
+#define F0R(i, a) for (int i=0; i<(a); i++)
+#define FORd(i,a,b) for (int i = (b)-1; i >= a; i--)
+#define F0Rd(i,a) for (int i = (a)-1; i >= 0; i--)
+#define trav(a,x) for (auto& a : x)
+#define uid(a, b) uniform_int_distribution<int>(a, b)(rng)
+ 
+#define sz(x) (int)(x).size()
+#define mp make_pair
+#define pb push_back
+#define f first
+#define s second
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+#define ins insert
+ 
+template<class T> bool ckmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+ 
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+ 
+const int MOD = 1000000007;
+const char nl = '\n';
+const int MX = 100001; //check the limits, dummy
+ 
+void merge(int arr[], int l, int m, int r)
+{
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 =  r - m;
+ 
+    /* Tạo các mảng tạm */
+    int L[n1], R[n2];
+ 
+    /* Copy dữ liệu sang các mảng tạm */
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1+ j];
+ 
+    /* Gộp hai mảng tạm vừa rồi vào mảng arr*/
+    i = 0; // Khởi tạo chỉ số bắt đầu của mảng con đầu tiên
+    j = 0; // Khởi tạo chỉ số bắt đầu của mảng con thứ hai
+    k = l; // IKhởi tạo chỉ số bắt đầu của mảng lưu kết quả
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= R[j])
+        {
+            arr[k] = L[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+ 
+    /* Copy các phần tử còn lại của mảng L vào arr nếu có */
+    while (i < n1)
+    {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+ 
+    /* Copy các phần tử còn lại của mảng R vào arr nếu có */
+    while (j < n2)
+    {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+ 
+/* l là chỉ số trái và r là chỉ số phải của mảng cần được sắp xếp */
+void mergeSort(int arr[], int l, int r)
+{
+    if (l < r)
+    {
+        // Tương tự (l+r)/2, nhưng cách này tránh tràn số khi l và r lớn
+        int m = l+(r-l)/2;
+ 
+        // Gọi hàm đệ quy tiếp tục chia đôi từng nửa mảng
+        mergeSort(arr, l, m);
+        mergeSort(arr, m+1, r);
+ 
+        merge(arr, l, m, r);
+    }
+}
+ 
+ 
+/* Hàm xuất mảng */
+void printArray(int A[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", A[i]);
+    printf("\n");
+}
+ 
+ 
+int main() {
+    
+    int arr[] = {12, 11, 13, 5, 6, 7};
+    int arr_size = sizeof(arr)/sizeof(arr[0]);
+ 
+    printf("Given array is \n");
+    printArray(arr, arr_size);
+ 
+    mergeSort(arr, 0, arr_size - 1);
+ 
+    printf("\nSorted array is \n");
+    printArray(arr, arr_size);
+    return 0;
+ 
+	return 0;
+}
---
+++ Algorithm/Sort/Sort_Basic.cpp
@@ -0,0 +1,51 @@
+﻿#include <bits/stdc++.h> 
+using namespace std; 
+
+void insertSort(int a[], int n)
+{
+
+    for(int i=0; i<n; i++){
+        
+        int tam = a[i]; 
+        int j = i - 1;
+
+        while( j>=0 && a[j] > tam){
+            // j = 0
+            a[j+1] = a[j]; 
+            j = j - 1; 
+            // j -- ; j = -1
+        }
+        a[j+1] = tam;
+
+    }
+}
+
+void bullesort(int a[], int n){
+
+    for (int i=0; i<=n-1; i++ ){
+
+            for(int j=n-1; j>i; j--){
+                if( a[j-1] > a[j])
+                    swap(a[j],a[j-1]);  
+            }
+    }
+}
+
+
+int main(){
+    int x[] = { 5, 25, 16, 8, 5, 1, 33}; 
+    int size = sizeof(x) / sizeof(x[0]); 
+    cout << "\nmang trc khi sap xep \n"; 
+    for(int i=0; i<size ; i++)
+    {
+        cout << x[i] << " "; 
+    } 
+    bullesort(x ,size);
+    cout << "\nmang sau khi sap xep \n"; 
+    for(int i=0; i<size ; i++)
+    {
+        cout << x[i] << " "; 
+    } 
+    cout << "\n\n\n\n"; 
+    return 0; 
+}
