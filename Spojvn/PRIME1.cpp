bc63a3b072c1e05654f12f15c1f67caad2e5ce59
duymanhdoan <doanmanhduy.yb0210@gmail.com>
2021-10-25 16:59:16 +0700

    add problem spojvn

---
+++ Spojvn/PRIME1.cpp
@@ -0,0 +1,39 @@
+﻿#include <bits/stdc++.h> 
+using namespace std; 
+
+#define ll long long 
+#define nl endl 
+void seive_LR(ll L, ll R) 
+{
+    vector <bool> isPrime(R - L + 1, true); 
+
+    for(ll i = 2; i * i <= R; ++i)
+    {
+        for(ll j = max(i * i, (L + i - 1)/ i * i); j <= R; j+=i )
+        {
+            isPrime[j - L] = false; 
+        }
+    }
+    if( 1 >= L) isPrime[1 - L] = false; 
+    
+    for(ll x = L;  x <= R; x++ )
+    {
+        if (isPrime[x - L])
+        {
+            cout << x << nl; 
+        }
+    }
+
+}
+
+int main()
+{
+    int T; cin >> T; 
+    while(T--)
+    {
+        ll l , r; 
+        cin >> l >> r; 
+        seive_LR(l ,r); 
+    }
+    return 0; 
+}
---
+++ Spojvn/sophongphu.cpp
@@ -0,0 +1,40 @@
+﻿#include <bits/stdc++.h> 
+using namespace std; 
+
+#define ll long long 
+#define nl endl 
+const int maxn = 1e5 + 10; 
+int isPrime[maxn]; 
+
+void seive(int maxn) 
+{
+    for(int i = 2; i*i <= maxn ; ++i)
+     {
+         if(isPrime[i]==0)
+         {
+             for(int j = i*i; j <= maxn; j+=i)
+                if(isPrime[j] == 0) 
+                    isPrime[j] = i; 
+         }
+     }
+    for( int i = 2; i <= maxn;++i)
+        if(isPrime[i] == 0)
+            isPrime[i] = i; 
+    for(int i=2; i<=maxn; ++i) 
+        cout << isPrime[i] <<" "; 
+}
+
+int main()
+{
+    // int T; cin >> T; 
+    seive(200); 
+    int L, R, cnt = 0; cin >> L >> R; 
+    for(int i = max(12,L); i<= R; ++i)
+    {
+        int s = 1; 
+        while()
+        if(s > i) cnt++;  
+    }
+    cout << cnt << endl; 
+    return 0; 
+}
