c3cfad053eb26ff350454c4c28736aaf10cd35bc
duymanhdoan <doanmanhduy.yb0210@gmail.com>
2021-10-27 10:32:11 +0700

    rename folder

---
+++ AlgorithmDS/BinarySearch/README.md
@@ -0,0 +1,6 @@
+﻿# Binary Search Algorithm
+
+### Lý Thuyết 
+
+
+### Problem 
---
+++ AlgorithmDS/SieveOfEratosthenes/primeFactorization.cpp
@@ -0,0 +1,50 @@
+﻿#include <bits/stdc++.h> 
+using namespace  std; 
+const int maxn = 10e7; 
+int p[maxn]; 
+int minPrime[100000];
+void  sevie(int n){
+
+     for(int i = 2; i*i <= n ; ++i)
+     {
+         if(minPrime[i]==0)
+         {
+             for(int j = i*i; j <= n; j+=i)
+                if(minPrime[j] == 0) 
+                    minPrime[j] = i; 
+         }
+     }
+    for( int i = 2; i <= n;++i)
+        if(minPrime[i] == 0)
+            minPrime[i] = i; 
+    for( int i = 2; i <= n;++i)
+        cout << minPrime[i] <<" "; 
+    cout << endl; 
+}
+
+vector <int> factorize_seive(int n)
+{
+    sevie(n); 
+    for(int i=0; i<=n+10; ++i)
+    {
+         cout <<"[" << i << "]="<<minPrime[i]<<" ";
+         if(i%10==0 && i > 1) cout << endl; 
+    } 
+    vector <int> res; 
+    while(n!=1)
+    {
+        int snt = minPrime[n]; 
+        res.push_back(snt); 
+        n /= snt; 
+        cout << snt << "*"; 
+    }
+    return res; 
+}
+
+int main()
+{
+    vector <int> v = factorize_seive(500); 
+
+    cout << "\n\n\n";
+    return 0; 
+}
